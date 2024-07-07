1bf6b877480db08c9dc6ad67559d663bcb8a951c
Le_Chat_Blanc <108530916+freeman649@users.noreply.github.com>
2024-07-07 17:38:47 +0200

    Add files via upload

---
+++ Spicetify Tool/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Spicetify_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2CircleButton2 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Remove_Spicetify = new Guna.UI2.WinForms.Guna2Button();
+            this.Install = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            this.Spotify_version_check = new Guna.UI2.WinForms.Guna2Button();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 11;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2CircleButton1
+            // 
+            this.guna2CircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.Location = new System.Drawing.Point(473, 4);
+            this.guna2CircleButton1.Name = "guna2CircleButton1";
+            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton1.Size = new System.Drawing.Size(17, 17);
+            this.guna2CircleButton1.TabIndex = 2;
+            this.guna2CircleButton1.Text = "guna2CircleButton1";
+            this.guna2CircleButton1.Click += new System.EventHandler(this.guna2CircleButton1_Click);
+            // 
+            // guna2CircleButton2
+            // 
+            this.guna2CircleButton2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButton2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButton2.FillColor = System.Drawing.Color.Yellow;
+            this.guna2CircleButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton2.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton2.Location = new System.Drawing.Point(452, 4);
+            this.guna2CircleButton2.Name = "guna2CircleButton2";
+            this.guna2CircleButton2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton2.Size = new System.Drawing.Size(17, 17);
+            this.guna2CircleButton2.TabIndex = 3;
+            this.guna2CircleButton2.Text = "guna2CircleButton2";
+            this.guna2CircleButton2.Click += new System.EventHandler(this.guna2CircleButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(1, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Spicetify Tool";
+            // 
+            // Remove_Spicetify
+            // 
+            this.Remove_Spicetify.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Remove_Spicetify.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Remove_Spicetify.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Remove_Spicetify.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Remove_Spicetify.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Remove_Spicetify.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Remove_Spicetify.ForeColor = System.Drawing.Color.White;
+            this.Remove_Spicetify.Location = new System.Drawing.Point(30, 281);
+            this.Remove_Spicetify.Name = "Remove_Spicetify";
+            this.Remove_Spicetify.Size = new System.Drawing.Size(437, 73);
+            this.Remove_Spicetify.TabIndex = 5;
+            this.Remove_Spicetify.Text = "Remove Spicetify";
+            this.Remove_Spicetify.Click += new System.EventHandler(this.Remove_Spicetify_Click);
+            // 
+            // Install
+            // 
+            this.Install.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Install.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Install.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Install.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Install.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(164)))), ((int)(((byte)(91)))));
+            this.Install.Font = new System.Drawing.Font("Segoe Print", 14.25F);
+            this.Install.ForeColor = System.Drawing.Color.White;
+            this.Install.Location = new System.Drawing.Point(30, 54);
+            this.Install.Name = "Install";
+            this.Install.Size = new System.Drawing.Size(437, 73);
+            this.Install.TabIndex = 6;
+            this.Install.Text = "Install Spicetify";
+            this.Install.Click += new System.EventHandler(this.Install_Click);
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.Location = new System.Drawing.Point(-3, 23);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(502, 10);
+            this.guna2Separator1.TabIndex = 8;
+            // 
+            // Spotify_version_check
+            // 
+            this.Spotify_version_check.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Spotify_version_check.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Spotify_version_check.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Spotify_version_check.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Spotify_version_check.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(66)))), ((int)(((byte)(137)))));
+            this.Spotify_version_check.Font = new System.Drawing.Font("Segoe Print", 14.25F);
+            this.Spotify_version_check.ForeColor = System.Drawing.Color.White;
+            this.Spotify_version_check.Location = new System.Drawing.Point(30, 168);
+            this.Spotify_version_check.Name = "Spotify_version_check";
+            this.Spotify_version_check.Size = new System.Drawing.Size(437, 73);
+            this.Spotify_version_check.TabIndex = 9;
+            this.Spotify_version_check.Text = "Verify Spotify Version";
+            this.Spotify_version_check.Click += new System.EventHandler(this.Spotify_version_check_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Comic Sans MS", 12.25F);
+            this.status.ForeColor = System.Drawing.Color.White;
+            this.status.Location = new System.Drawing.Point(16, 390);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(112, 24);
+            this.status.TabIndex = 10;
+            this.status.Text = "Status: Wait";
+            this.status.Click += new System.EventHandler(this.status_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(495, 459);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.Spotify_version_check);
+            this.Controls.Add(this.guna2Separator1);
+            this.Controls.Add(this.Install);
+            this.Controls.Add(this.Remove_Spicetify);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2CircleButton2);
+            this.Controls.Add(this.guna2CircleButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton2;
+        private Guna.UI2.WinForms.Guna2Button Install;
+        private Guna.UI2.WinForms.Guna2Button Remove_Spicetify;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+        private Guna.UI2.WinForms.Guna2Button Spotify_version_check;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ Spicetify Tool/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spicetify_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2CircleButton1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private async void Install_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                status.Text = "Status: Checking Spotify version";
+
+                bool isMicrosoftStoreVersion = IsMicrosoftStoreVersionInstalled();
+                if (isMicrosoftStoreVersion)
+                {
+                    status.Text = "Status: Uninstalling Microsoft Store version of Spotify";
+                    await UninstallMicrosoftStoreVersion();
+                }
+
+                status.Text = "Status: Installing standalone version of Spotify";
+                await InstallStandaloneVersion();
+
+                status.Text = "Status: Installing Spicetify";
+                await ExecutePowerShellCommandAsync("iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex");
+
+                status.Text = "Status: Installing Spicetify Marketplace";
+                await ExecutePowerShellCommandAsync("iwr -useb https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1 | iex");
+
+                status.Text = "Status: Installation Complete";
+                MessageBox.Show("Spicetify successfully installed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                status.Text = "Status: Error during installation";
+                MessageBox.Show($"Error during installation: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool IsMicrosoftStoreVersionInstalled()
+        {
+            string msStorePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Packages\SpotifyAB.SpotifyMusic_zpdnekdrzrea0";
+            return System.IO.Directory.Exists(msStorePath);
+        }
+
+        private Task UninstallMicrosoftStoreVersion()
+        {
+            return Task.Run(() =>
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"Get-AppxPackage *SpotifyAB.SpotifyMusic* | Remove-AppxPackage\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(psi))
+                {
+                    process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                    process.BeginOutputReadLine();
+
+                    process.ErrorDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                    process.BeginErrorReadLine();
+
+                    process.WaitForExit();
+                }
+            });
+        }
+
+        private Task InstallStandaloneVersion()
+        {
+            return Task.Run(() =>
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"Invoke-WebRequest -Uri https://download.scdn.co/SpotifySetup.exe -OutFile $env:USERPROFILE\\Downloads\\SpotifySetup.exe; Start-Process -FilePath $env:USERPROFILE\\Downloads\\SpotifySetup.exe -Wait\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(psi))
+                {
+                    process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                    process.BeginOutputReadLine();
+
+                    process.ErrorDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                    process.BeginErrorReadLine();
+
+                    process.WaitForExit();
+                }
+            });
+        }
+
+        private Task ExecutePowerShellCommandAsync(string command)
+        {
+            return Task.Run(() =>
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{command}\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(psi))
+                {
+                    process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                    process.BeginOutputReadLine();
+
+                    process.ErrorDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                    process.BeginErrorReadLine();
+
+                    process.WaitForExit();
+                }
+            });
+        }
+
+        private void Spotify_version_check_Click(object sender, EventArgs e)
+        {
+            string version = CheckSpotifyVersion();
+            status.Text = version;
+        }
+        private string CheckSpotifyVersion()
+        {
+            if (IsMicrosoftStoreVersionInstalled2())
+            {
+                return "Status: Microsoft Store version of Spotify is installed.";
+            }
+            else if (IsStandaloneVersionInstalled())
+            {
+                return "Status: Standalone version of Spotify is installed.";
+            }
+            else
+            {
+                return "Status: No version of Spotify is installed.";
+            }
+        }
+
+        private bool IsMicrosoftStoreVersionInstalled2()
+        {
+            string msStorePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Packages\SpotifyAB.SpotifyMusic_zpdnekdrzrea0";
+            return System.IO.Directory.Exists(msStorePath);
+        }
+
+        private bool IsStandaloneVersionInstalled()
+        {
+            string standalonePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Spotify";
+            return System.IO.Directory.Exists(standalonePath);
+        }
+
+        private async void Remove_Spicetify_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                status.Text = "Status: Removing Spicetify";
+                await ExecutePowerShellCommandAsync("spicetify restore");
+                await ExecutePowerShellCommandAsync("Remove-Item -Recurse -Force $env:APPDATA\\spicetify");
+                await ExecutePowerShellCommandAsync("Remove-Item -Recurse -Force $env:LOCALAPPDATA\\spicetify");
+
+                status.Text = "Status: Spicetify removed successfully";
+                MessageBox.Show("Spicetify removed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                status.Text = "Status: Error during removal";
+                MessageBox.Show($"Error during removal: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void status_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CircleButton2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
